ae27bb0377c32cceb9cd0032cee1729cdb8491b1
Cyrus Neary <cyrusneary@gmail.com>
2022-03-06 17:39:21 -0600

    Adding labyrinth demonstration files and code to use behavioral cloning.

---
+++ examples/imitation_unity_labyrinth.py
@@ -0,0 +1,68 @@
+import pathlib
+# import pickle
+import tempfile
+# import os
+
+import stable_baselines3 as sb3
+from stable_baselines3.common.utils import obs_as_tensor
+
+from imitation.algorithms import adversarial, bc
+from imitation.util import logger
+
+import sys, os
+sys.path.append('..')
+from utils.demo_loader import load_and_parse_unity_demo
+from Environments.unity_labyrinth import build_unity_labyrinth_env
+
+
+
+
+tempdir = tempfile.TemporaryDirectory(prefix="quickstart")
+tempdir_path = pathlib.Path(tempdir.name)
+print(f"All Tensorboards and logging are being written inside {tempdir_path}/.")
+
+# Load the unity demo file
+file_path = os.path.abspath('../demos/labyrinth/IRLnonlava.demo')
+transitions = load_and_parse_unity_demo(demo_file=file_path, sequence_length=3382)
+
+# Load the unity labyrinth environment.
+env_settings = {
+    'time_scale' : 99.0,
+}
+
+env, side_channels = build_unity_labyrinth_env()
+side_channels['engine_config_channel'].set_configuration_parameters(
+                                        time_scale=env_settings['time_scale'])
+
+# Train BC on expert data.
+# BC also accepts as `demonstrations` any PyTorch-style DataLoader that iterates over
+# dictionaries containing observations and actions.
+bc_logger = logger.configure(tempdir_path / "BC/")
+bc_trainer = bc.BC(
+    observation_space=env.observation_space,
+    action_space=env.action_space,
+    expert_data=transitions,
+    # custom_logger=bc_logger,
+)
+bc_trainer.train(n_epochs=1)
+
+print(bc_trainer)
+
+# Test the results.
+n_episodes = 10
+n_steps = 3500
+
+side_channels['engine_config_channel']\
+            .set_configuration_parameters(time_scale=1.0)
+
+# Set the environment task to be the overall composite task
+side_channels['custom_side_channel'].send_string('-1,-1')
+
+for episode_ind in range(n_episodes):
+    obs = env.reset()
+    # self.reset(side_channels)
+    for step in range(n_steps):
+        action, _states = bc_trainer.policy.forward(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
+        if done:
+            break
--- utils/demo_loader.py
+++ utils/demo_loader.py
@@ -1,23 +1,94 @@
-from mlagents.trainers.demo_loader import demo_to_buffer
-import os
+import mlagents
+import imitation
 
-def load_demo_obs_and_actions(demo_file:str, sequence_length:int):
+from mlagents.trainers.demo_loader import demo_to_buffer
+from imitation.data.types import Transitions
+import os
+import numpy as np
+
+def load_and_parse_unity_demo(demo_file:str, sequence_length:int) \
+                                    -> imitation.data.types.Transitions:
+    """
+    Load a unity .demo protobuffer file and parse it into a form usable by 
+    the algorithms within the "imitation" python package.
+
+    Arguments
+    ---------
+    demo_file:
+        The path to the .demo file in question.
+    sequence_length:
+        The length of the trajectories to fill the buffer.
+
+    Returns
+    -------
+    transitions:
+        A imitation.data.types.Transitions object storing the demonstration
+        data in a format that can be used by the "imitation" library.
+    """
     file_path = os.path.abspath(demo_file)
 
-    behavior_spec, agent_buffer = demo_to_buffer(file_path=file_path, sequence_length=sequence_length)
+    _, agent_buffer = demo_to_buffer(file_path=file_path, 
+                                        sequence_length=sequence_length)
+    return parse_agent_buffer(agent_buffer=agent_buffer)
+
+def parse_agent_buffer(agent_buffer:mlagents.trainers.buffer.AgentBuffer) \
+                                        -> imitation.data.types.Transitions:
+    """
+    Parse a Unity mlagents AgentBuffer object into a Transitions object
+    from the imitation learning package.
+
+    Arguments
+    ---------
+    agent_buffer:
+        The agent buffer to be parsed.
+    
+    Returns
+    -------
+    transitions:
+        The output Transitions object.
+    """
     items = list(agent_buffer._fields.items())
 
-    # Get observations
+    for key, val in items:
 
-    # Get actions
+        if type(key) is not tuple:
 
+            # Get "dones"
+            if key.name == 'DONE':
+                dones = np.array(val, dtype=bool)
+
+            # Get actions
+            if key.name == 'CONTINUOUS_ACTION':
+                acts = np.array(val)
+
+        # Get observations
+        elif (key[0].name == 'OBSERVATION'):
+            obs = np.array(val)
+
+    next_obs = obs[1::]
+    obs = obs[0:-1]
+    dones = dones[0:-1]
+    acts = acts[0:-1]
+    infos = np.array([{}] * obs.shape[0])
+
+    transitions_dict = {
+        'obs' : obs,
+        'dones' : dones,
+        'acts' : acts,
+        'next_obs' : next_obs,
+        'infos' : infos,
+    }
+
+    return Transitions(**transitions_dict)
 
 def main():
-    file_path = os.path.abspath('../demos/crawler/ExpertCrawler.demo')
+    file_path = os.path.abspath('../demos/labyrinth/IRLnonlava.demo')
 
-    behavior_spec, agent_buffer = demo_to_buffer(file_path=file_path, sequence_length=10011)
-    items = list(agent_buffer._fields.items())
-    print(agent_buffer)
+    output = load_and_parse_unity_demo(demo_file=file_path, sequence_length=3382)
+
+    # behavior_spec, agent_buffer = demo_to_buffer(file_path=file_path, sequence_length=3382)
+    # items = list(agent_buffer._fields.items())
+    print(output)
 
 if __name__ == '__main__':
     main()
